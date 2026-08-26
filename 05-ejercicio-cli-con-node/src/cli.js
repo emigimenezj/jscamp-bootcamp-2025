fb3019bbfc032d1ce13d85c15de3d1621d40bfc6
madeval <alvarez.fing@gmail.com>
2026-08-26 12:35:01 -0300

    feat: actualizar manejo de exitCode para usar EXIT_CODES.APP como fallback

--- 05-ejercicio-cli-con-node/src/cli.js
+++ 05-ejercicio-cli-con-node/src/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 import { app } from "./app/run-cli.js";
+import { EXIT_CODES } from "./constants/exit-codes.js";
 import { renderError } from "./output/render-feedback.js";
 
 try {
   await app(process.argv.slice(2));
 } catch (error) {
   renderError(error);
-  process.exitCode = error.exitCode ?? 1;
+  process.exitCode = error.exitCode ?? EXIT_CODES.APP; // Agregamos exitCode como fallback por si error.exitCode no devuelve el esperado
 }
