e932a8bc7380e259d624d2b6c58e255f6a283788
madeval <alvarez.fing@gmail.com>
2026-09-09 18:02:33 -0300

    Renombrar middleware de CORS para mejorar la claridad en app.js

--- 07-ejercicio-api-rest-con-express-y-mvc/app.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { DEFAULTS } from "./config.js";
-import { middleware } from "./middlewares/cors.js";
-import { logger, error } from "./middlewares/logger.js";
+import { corsMiddleware } from "./middlewares/cors.js";
+import { error, logger } from "./middlewares/logger.js";
 
 import { jobsRouter } from "./routes/jobs.js";
 
 const app = express();
 
 app.use(logger);
-app.use(middleware.cors);
+app.use(corsMiddleware); // <- Implementamos el middleware con su nuevo nombre
 app.use(express.json());
 
 app.use("/jobs", jobsRouter);
