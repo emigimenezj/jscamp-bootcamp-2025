3e28b6dc629478e633060f0bf0b2040d17c0c415
madeval <alvarez.fing@gmail.com>
2026-09-09 18:06:23 -0300

    Renombrar variable de orígenes a ACCEPTED_ORIGINS para mayor claridad y exportar middleware de CORS de manera más explícita

--- 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 
-const origins = [
+// Hicimos un cambio de nombre a mayuscula para que quede claro que es una constante no mutable. Es un cambio que no afecta el antes/después pero me gusta hacerlo para diferenciar variables de escritura y de lectura.
+const ACCEPTED_ORIGINS = [
   "http://localhost:3000",
   "http://localhost:1234",
   "https://midu.dev",
@@ -8,8 +9,6 @@ const origins = [
   "http://localhost:5173",
 ];
 
-export const middleware = {
-  cors: cors({
-    origin: origins,
-  }),
-};
+// Muy buena idea crear un objeto middleware para luego exportar todos los middleware en formato key/value dentro.
+// En estos casos en los que queremos responsabilizar un archivo a una sola tarea, lo mejor es exportarlo como lo que es, el middleware encargado de los cors.
+export const corsMiddleware = cors({ origin: ACCEPTED_ORIGINS });
