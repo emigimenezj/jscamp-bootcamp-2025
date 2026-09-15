8f61d960b984b5f24103e905b972f862262ccbfe
Emilio Maximiliano Giménez Jofre <emigimenezj@zohomail.com>
2026-09-15 03:33:37 +0000

    ci: add artificial comment to main.jsx for CI/CD testing

--- 12-ejercicio-cicd/projects/frontend/src/main.jsx
+++ 12-ejercicio-cicd/projects/frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 
+// Comentario completamente artificial para probar CI/CD. 😁
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
