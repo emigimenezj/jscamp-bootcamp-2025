5209aead8ad747bec4340df71d999fcfd91a0ebe
madeval <alvarez.fing@gmail.com>
2026-09-16 19:11:34 -0300

    Requerir el campo 'data' en el esquema de trabajo para asegurar la funcionalidad de los filtros en las búsquedas

--- 08-ejercicio-testing-con-node-y-zod/schemas/jobs.js
+++ 08-ejercicio-testing-con-node-y-zod/schemas/jobs.js
@@ -5,11 +5,19 @@ const jobSchema = z.object({
   empresa: z.string(),
   ubicacion: z.string(),
   descripcion: z.string().optional(),
+  /*
   data: z.object({
     technology: z.array(z.string()),
     modalidad: z.string().optional(),
     nivel: z.string().optional(),
   }).optional(),
+  */
+  // Si data es opcional, los filtros que hacemos en GET ?/jobs buscando data.technology no funcionarían. Lo ideal es dejar `data` como requerido y los campos opcionales como opcionales (modalidad y nivel)
+  data: z.object({
+    technology: z.array(z.string()),
+    modalidad: z.string().optional(),
+    nivel: z.string().optional(),
+  }),
   content: z.unknown().optional(),
 })
 
